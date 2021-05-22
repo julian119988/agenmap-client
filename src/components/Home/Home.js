61fe84e8d3cec146a617cb9e0485a6193de64788
Julian <julian.zambroni@gmail.com>
2021-05-22 21:27:40 +0000

    Nightly commit v3

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -48,7 +48,7 @@ export default function Home() {
         setFilteredStays(finalArray);
     }
     function setLocationOnClick(event) {
-        setLocation(event.target.lastChild.nodeValue); // continue here tomorrow
+        setLocation(event.target.lastChild.nodeValue); // continue here tomorro
     }
     async function fetchData() {
         return await db.collection("stays").get();
