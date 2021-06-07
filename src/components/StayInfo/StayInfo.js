c6cc181bfec38fc9df313a73d80adfcc951a9c8e
Julian Zambroni <julian.zambroni@gmail.com>
2021-06-07 20:25:08 -0300

    Nightly commit 07062021

--- src/components/StayInfo/StayInfo.js
+++ src/components/StayInfo/StayInfo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Loader from "react-loader-spinner";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
@@ -7,24 +7,171 @@ import starIcon from "../../images/star.svg";
 import PinIcon from "../../services/PinIconColorChanger";
 import homeIcon from "../../images/homeIcon.svg";
 import bedIcon from "../../images/bed.svg";
+import { UserContext } from "../../App";
+import { store } from "react-notifications-component";
 
 export default function StayInfo() {
   const [data, setData] = useState(undefined);
+  const [isSaved, setIsSaved] = useState(null);
   const { id } = useParams();
+  const user = useContext(UserContext);
+
   useEffect(() => {
     fetchStay(); // eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .where("email", "==", user.email)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            let isNew = true;
+            doc.data().stays.forEach((element) => {
+              if (element === id) {
+                isNew = false;
+              }
+            });
+            if (isNew) {
+              setIsSaved(false);
+            } else {
+              setIsSaved(true);
+            }
+          });
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [user]);
   async function fetchStay() {
     const response = await db.collection("stays").doc(id).get();
-    console.log(response.data());
+
     setData(response.data());
   }
+
+  function checkUser() {
+    if (!user) {
+      store.addNotification({
+        title: "Error!",
+        message: "You need to log in!",
+        type: "danger",
+        insert: "top",
+        container: "top-center",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3500,
+        },
+        slidingEnter: {
+          swipe: {
+            duration: 400,
+            timingFunction: "ease-out",
+            delay: 0,
+          },
+          fade: {
+            duration: 400,
+            timingFunction: "ease-out",
+            delay: 0,
+          },
+        },
+      });
+    } else {
+      manageStay();
+    }
+  }
+  function saveStay() {
+    setIsSaved(null);
+    db.collection("users")
+      .where("email", "==", user.email)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          let isNew = true;
+          doc.data().stays.forEach((element) => {
+            if (element === id) {
+              isNew = false;
+            }
+          });
+          if (isNew) {
+            db.collection("users")
+              .doc(doc.id)
+              .set(
+                {
+                  stays: [...doc.data().stays, id],
+                },
+                { merge: true }
+              )
+              .then(() => {
+                setIsSaved(true);
+                console.log("Document successfully written!");
+              })
+              .catch((error) => {
+                console.error("Error writing document: ", error);
+              });
+          }
+        });
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }
+
+  function deleteStay() {
+    setIsSaved(null);
+    db.collection("users")
+      .where("email", "==", user.email)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          let isNew = true;
+          doc.data().stays.forEach((element) => {
+            if (element === id) {
+              isNew = false;
+            }
+          });
+          if (!isNew) {
+            let newStays = doc.data().stays;
+            let index = newStays.findIndex((element) => element === id);
+            newStays.splice(index, 1);
+            db.collection("users")
+              .doc(doc.id)
+              .set(
+                {
+                  stays: newStays,
+                },
+                { merge: true }
+              )
+              .then(() => {
+                setIsSaved(false);
+                console.log("Document successfully deleted!");
+              })
+              .catch((error) => {
+                console.error("Error deleting document: ", error);
+              });
+          }
+        });
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }
+
+  function manageStay() {
+    if (isSaved === false) {
+      saveStay();
+    } else if (isSaved === true) {
+      deleteStay();
+    }
+  }
+
   return (
     <Main>
       {data === undefined ? (
         <Center>
-          <Loader type="TailSpin" color="#00BFFF" height={80} width={80} />
+          <Loader type="TailSpin" color="#00BFFF" />
         </Center>
       ) : (
         <>
@@ -32,16 +179,39 @@ export default function StayInfo() {
             <StayTitle>{data.title}</StayTitle>
           </Container>
           <Container center width>
-            <StaysImg src={data.photo} alt={data.title} />
+            <StaysImgDiv>
+              <StaysImg src={data.photo} alt={data.title}></StaysImg>
+              {isSaved === null ? (
+                <LoadingDiv>
+                  <Loader
+                    type="TailSpin"
+                    color="#00BFFF"
+                    heigh={25}
+                    width={25}
+                  />
+                </LoadingDiv>
+              ) : isSaved ? (
+                <SaveButton onClick={checkUser} save></SaveButton>
+              ) : (
+                <SaveButton onClick={checkUser}></SaveButton>
+              )}
+            </StaysImgDiv>
           </Container>
-          <Container row padding center width between>
+          <Container
+            row
+            padding
+            center
+            width
+            between
+            superHost={data.superHost}
+            rating
+          >
             {data.superHost ? (
               <SuperHost>
                 <SuperHostText>Super Host</SuperHostText>
               </SuperHost>
             ) : null}
-
-            <Container row center>
+            <Container row center left>
               <StarIcon src={starIcon} alt="Red start icon(rating)"></StarIcon>
               <Rating>{data.rating}</Rating>
             </Container>
@@ -52,7 +222,7 @@ export default function StayInfo() {
             </Container>
             <Type style={{ marginLeft: "7px" }}>{data.type}</Type>
           </Container>
-          <Container row padding width between center>
+          <Container padding width row center>
             <Container row center>
               <Container style={{ width: "25px", height: "25px" }}>
                 <img src={bedIcon} alt="Bed icon" />
@@ -63,6 +233,8 @@ export default function StayInfo() {
                   : `${data.beds} beds`}
               </Type>
             </Container>
+          </Container>
+          <Container padding width row center>
             <Container row center>
               <Container style={{ width: "25px", height: "25px" }}>
                 <PinIcon color="rgba(235,87,87,0.9)" />
@@ -77,6 +249,40 @@ export default function StayInfo() {
     </Main>
   );
 }
+// prettier-ignore
+const SaveButton = styled.button`
+  background-color: ${(props) => props.theme.button.backgroundColor};
+  position: absolute;
+  bottom: 10%;
+  right: 10%;
+  border: none;
+  outline: none;
+  border-radius: 50%;
+  width: calc(${(props) => props.theme.vh} * 4px);
+  height: calc(${(props) => props.theme.vh} * 4px);
+  cursor: pointer;
+  border: 1px solid rgb(235, 87, 87);
+  background-image: url(${(props) => props.save ? props.theme.delete : props.theme.save});
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+const LoadingDiv = styled.div`
+  background-color: ${(props) => props.theme.button.backgroundColor};
+  position: absolute;
+  bottom: 10%;
+  right: 10%;
+  border: none;
+  outline: none;
+  border-radius: 50%;
+  width: calc(${(props) => props.theme.vh} * 4px);
+  height: calc(${(props) => props.theme.vh} * 4px);
+  cursor: pointer;
+  border: 1px solid rgb(235, 87, 87);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Main = styled.div`
   min-height: calc(${(props) => props.theme.vh} * 100px);
   display: flex;
@@ -86,11 +292,15 @@ const Main = styled.div`
 `;
 const Container = styled.div`
   display: flex;
+  top: calc(${(props) => props.theme.vh} * 42.7px);
   flex-direction: ${(props) => (props.row ? "row" : "column")};
   ${(props) => (props.center ? "align-items: center;" : null)};
   ${(props) => (props.padding ? "padding: 15px 30px 15px 30px;" : null)}
   ${(props) => (props.width ? "width: 100%;" : null)}
   ${(props) => (props.between ? "justify-content: space-between;" : null)}
+  ${(props) => (props.left ? "margin-left: auto;" : null)}
+  ${(props) =>
+    !props.superHost && props.rating ? "position: absolute;" : null}
 `;
 const Center = styled.div`
   display: flex;
@@ -99,15 +309,23 @@ const Center = styled.div`
   width: 100%;
   flex: 1;
 `;
-const StaysImg = styled.img`
+const StaysImgDiv = styled.div`
   border-radius: 24px;
   width: 90%;
   height: 250px;
-  object-fit: cover;
-  ${(props) => props.theme.image.filter}
   z-index: 0;
   margin: 10px 0 10px 0;
+  position: relative;
 `;
+const StaysImg = styled.img`
+  ${(props) => props.theme.image.filter}
+  object-fit: cover;
+  border-radius: 24px;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+`;
+
 const SuperHostText = styled.h2`
   font-family: Montserrat;
   font-style: normal;
--- src/config/themes.js
+++ src/config/themes.js
@@ -1,80 +1,89 @@
+import saveDark from "../images/saveDark.svg";
+import saveLight from "../images/saveLight.svg";
+import deleteDark from "../images/deleteDark.svg";
+import deleteLight from "../images/deleteLight.svg";
+
 const theme = {
-    dark: {
-        vh: window.innerHeight * 0.01,
-        navbar: {
-            color: "white;",
-        },
-        info: {
-            color: "lightgray;",
-        },
-        button: {
-            backgroundColor: "rgba(235, 87, 87, 0.9);",
-            color: "aliceblue;",
-        },
-        title: {
-            color: "rgba(235, 87, 87, 0.9);",
-            editColor: "rgba(235, 87, 87, 0.9);",
-        },
-        superHost: {
-            color: "white;",
-            borderColor: "lightgray",
-        },
-        stayTitle: {
-            color: "aliceblue;",
-        },
-        input: {
-            color: "black",
-            backgroundColor: "aliceblue;",
-        },
-        li: {
-            color: "aliceblue",
-        },
-        image: {
-            filter: "filter: brightness(.8) contrast(1.2);",
-        },
-        backgroundColor: "#44484a;",
-        color: "white;",
-        borderColor: "rgba(235, 87, 87, 0.9);",
-        linkColor: "cornflowerblue",
+  dark: {
+    save: saveDark,
+    delete: deleteDark,
+    vh: window.innerHeight * 0.01,
+    navbar: {
+      color: "white;",
     },
-    light: {
-        vh: window.innerHeight * 0.01,
-        navbar: {
-            color: "rgba(235, 87, 87, 0.9);",
-        },
-        info: {
-            color: "gray;",
-        },
-        button: {
-            backgroundColor: "rgba(235, 87, 87, 0.9);",
-            color: "white;",
-        },
-        title: {
-            color: "rgba(235, 87, 87, 0.9);",
-            editColor: "black",
-        },
-        superHost: {
-            color: "#4f4f4f;",
-            borderColor: "#4f4f4f",
-        },
-        stayTitle: {
-            color: "#333333;",
-        },
-        input: {
-            color: "black",
-            backgroundColor: "white",
-        },
-        li: {
-            color: "black",
-        },
-        image: {
-            filter: "",
-        },
-        backgroundColor: "white;",
-        color: "black;",
-        borderColor: "transparent;",
-        linkColor: "royalblue",
+    info: {
+      color: "lightgray;",
     },
+    button: {
+      backgroundColor: "rgba(235, 87, 87, 0.9);",
+      color: "aliceblue;",
+    },
+    title: {
+      color: "rgba(235, 87, 87, 0.9);",
+      editColor: "rgba(235, 87, 87, 0.9);",
+    },
+    superHost: {
+      color: "white;",
+      borderColor: "lightgray",
+    },
+    stayTitle: {
+      color: "aliceblue;",
+    },
+    input: {
+      color: "black",
+      backgroundColor: "aliceblue;",
+    },
+    li: {
+      color: "aliceblue",
+    },
+    image: {
+      filter: "filter: brightness(.8) contrast(1.2);",
+    },
+    backgroundColor: "#44484a;",
+    color: "white;",
+    borderColor: "rgba(235, 87, 87, 0.9);",
+    linkColor: "cornflowerblue",
+  },
+  light: {
+    save: saveLight,
+    delete: deleteLight,
+    vh: window.innerHeight * 0.01,
+    navbar: {
+      color: "rgba(235, 87, 87, 0.9);",
+    },
+    info: {
+      color: "gray;",
+    },
+    button: {
+      backgroundColor: "rgba(235, 87, 87, 0.9);",
+      color: "white;",
+    },
+    title: {
+      color: "rgba(235, 87, 87, 0.9);",
+      editColor: "black",
+    },
+    superHost: {
+      color: "#4f4f4f;",
+      borderColor: "#4f4f4f",
+    },
+    stayTitle: {
+      color: "#333333;",
+    },
+    input: {
+      color: "black",
+      backgroundColor: "white",
+    },
+    li: {
+      color: "black",
+    },
+    image: {
+      filter: "",
+    },
+    backgroundColor: "white;",
+    color: "black;",
+    borderColor: "transparent;",
+    linkColor: "royalblue",
+  },
 };
 
 export default theme;
